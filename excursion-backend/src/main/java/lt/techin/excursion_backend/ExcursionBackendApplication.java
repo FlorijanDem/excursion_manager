d4e1dd26c65aa570eae68249e0b4a7155b3bee60
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2025-07-16 18:52:55 +0300

    Prepare frontend, backend projects

---
+++ excursion-backend/src/main/java/lt/techin/excursion_backend/ExcursionBackendApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.excursion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcursionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcursionBackendApplication.class, args);
+	}
+
+}
---
+++ excursion-backend/src/test/java/lt/techin/excursion_backend/ExcursionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.excursion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcursionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
